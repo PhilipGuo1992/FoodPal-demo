c893c4d6d793444729d8fc95644a996d5ddb281a
Yi Ren <yiren1105@gmail.com>
2018-02-10 18:31:12 -0400

    Firebase Settings

--- app/src/main/java/com/example/phili/foodpaldemo/LoginActivity.java
+++ app/src/main/java/com/example/phili/foodpaldemo/LoginActivity.java
@@ -3,11 +3,19 @@ package com.example.phili.foodpaldemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class LoginActivity extends AppCompatActivity {
 
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
+    //FirebaseDatabase database = FirebaseDatabase.getInstance();
+
 }
