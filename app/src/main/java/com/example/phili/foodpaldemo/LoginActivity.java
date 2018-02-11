eb4cf144a44193a86ac8460688c83bb82dd6a2ff
Unknown <philipguo1992@gmail.com>
2018-02-11 11:27:31 -0400

    fix conflict, merge it by hand

--- app/src/main/java/com/example/phili/foodpaldemo/LoginActivity.java
+++ app/src/main/java/com/example/phili/foodpaldemo/LoginActivity.java
@@ -8,26 +8,18 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
-<<<<<<< Updated upstream
-
-    
-=======
     // user register new account
     private TextView createAccount;
 
 
->>>>>>> Stashed changes
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
 
-<<<<<<< Updated upstream
-=======
 
-
->>>>>>> Stashed changes
     }
     //FirebaseDatabase database = FirebaseDatabase.getInstance();
 
