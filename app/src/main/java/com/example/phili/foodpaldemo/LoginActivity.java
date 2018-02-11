5c07dabbc92c80b0b0de0415292ca3e88847ef63
Unknown <philipguo1992@gmail.com>
2018-02-10 20:05:38 -0400

    Auto stash before merge of "develop" and "origin/develop"
    add user register UI

--- app/src/main/java/com/example/phili/foodpaldemo/LoginActivity.java
+++ app/src/main/java/com/example/phili/foodpaldemo/LoginActivity.java
@@ -2,19 +2,32 @@ package com.example.phili.foodpaldemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
+<<<<<<< Updated upstream
 
     
+=======
+    // user register new account
+    private TextView createAccount;
+
+
+>>>>>>> Stashed changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
 
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
     }
     //FirebaseDatabase database = FirebaseDatabase.getInstance();
 
