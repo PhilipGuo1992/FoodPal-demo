0ee233854430b4be9967177dc41249fb65836c63
Yi Ren <yiren1105@gmail.com>
2018-02-11 11:30:56 -0400

    firebase config

--- app/src/main/java/com/example/phili/foodpaldemo/LoginActivity.java
+++ app/src/main/java/com/example/phili/foodpaldemo/LoginActivity.java
@@ -8,7 +8,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class LoginActivity extends AppCompatActivity {
 
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
