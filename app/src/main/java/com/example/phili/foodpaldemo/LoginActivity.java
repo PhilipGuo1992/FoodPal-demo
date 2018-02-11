28d8e26df6d09a623fd8adf89bfdc0d500905c60
Yi Ren <yiren1105@gmail.com>
2018-02-11 14:01:06 -0400

    Login act and reg act

--- app/src/main/java/com/example/phili/foodpaldemo/LoginActivity.java
+++ app/src/main/java/com/example/phili/foodpaldemo/LoginActivity.java
@@ -1,15 +1,19 @@
 package com.example.phili.foodpaldemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 //import com.google.firebase.database.FirebaseDatabase;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     // user register new account
     private TextView createAccount;
+    private Button btnLogin;
 
 
 
@@ -19,8 +23,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        createAccount = (TextView) findViewById(R.id.app_create);
+        btnLogin = (Button) findViewById(R.id.btn_login);
 
+        createAccount.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        if (view == createAccount) {
+            startActivity(new Intent(this, RegisterActivity.class));
+        }
     }
     //FirebaseDatabase database = FirebaseDatabase.getInstance();
 
--- app/src/main/java/com/example/phili/foodpaldemo/RegisterActivity.java
+++ app/src/main/java/com/example/phili/foodpaldemo/RegisterActivity.java
@@ -1,20 +1,30 @@
 package com.example.phili.foodpaldemo;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button btnRegister;
     private EditText editTextEmail;
-    private EditText editTextPasswword;
+    private EditText editTextPassword;
     private TextView textViewLogin;
+    private ProgressDialog progressDialog;
 
-
+    private FirebaseAuth firebaseAuth;
 
 
     @Override
@@ -22,18 +32,60 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         btnRegister = (Button) findViewById(R.id.btn_register);
         editTextEmail = (EditText) findViewById(R.id.input_email);
-        editTextPasswword = (EditText) findViewById(R.id.input_password);
+        editTextPassword = (EditText) findViewById(R.id.input_password);
         textViewLogin = (TextView) findViewById(R.id.textViewLogin);
 
-        
+
+        btnRegister.setOnClickListener(this);
+        textViewLogin.setOnClickListener(this);
 
 
     }
 
+    public void UserRegister(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)) {
+            //If email is empty
+            Toast.makeText(this, "Please enter your Email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)) {
+            //If email is empty
+            Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Redirecting...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                        } else
+                        {
+                            Toast.makeText(RegisterActivity.this, "Could not register", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+    }
     @Override
     public void onClick(View view) {
-
+        if (view == btnRegister) {
+            UserRegister();
+        }
+        if (view == textViewLogin) {
+            //Flip to Login Page
+        }
     }
 }
