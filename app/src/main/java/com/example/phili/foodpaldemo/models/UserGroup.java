b8108996337e9c2d66b3daddf62a282881ccd266
Yi Ren <yiren1105@gmail.com>
2018-02-21 16:31:53 -0400

    Auto stash before merge of "develop" and "origin/develop"
    create logic

--- app/src/main/java/com/example/phili/foodpaldemo/models/UserGroup.java
+++ app/src/main/java/com/example/phili/foodpaldemo/models/UserGroup.java
@@ -10,12 +10,82 @@ public class UserGroup {
 
     private String groupID;
     private String groupName;
+    private String mealTime;
+    private String restaurantName;
 
     private String description;
+
+
 
     // one group can have many users
     private Set<User> currentUsers;
 
+    // default constructor
+    public UserGroup(){
 
+    }
 
+//    public UserGroup(String groupID, String groupName, String mealTime, String restaurantName, String description) {
+//        this.groupID = groupID;
+//        this.groupName = groupName;
+//        this.mealTime = mealTime;
+//        this.restaurantName = restaurantName;
+//        this.description = description;
+//    }
+    //Test only
+    public UserGroup(String groupName, String mealTime, String restaurantName) {
+        //this.groupID = groupID;
+        this.groupName = groupName;
+        this.mealTime = mealTime;
+        this.restaurantName = restaurantName;
+        //this.description = description;
+    }
+
+    public String getGroupID() {
+        return groupID;
+    }
+
+    public void setGroupID(String groupID) {
+        this.groupID = groupID;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getMealTime() {
+        return mealTime;
+    }
+
+    public void setMealTime(String mealTime) {
+        this.mealTime = mealTime;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<User> getCurrentUsers() {
+        return currentUsers;
+    }
+
+    public void setCurrentUsers(Set<User> currentUsers) {
+        this.currentUsers = currentUsers;
+    }
 }
