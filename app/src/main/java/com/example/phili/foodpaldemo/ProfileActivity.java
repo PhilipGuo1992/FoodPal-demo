2e095073877788828cc230f7539f94e9a1e59dbf
Yi Ren <yiren1105@gmail.com>
2018-02-24 21:19:23 -0400

    retrieve and updata profile

--- app/src/main/java/com/example/phili/foodpaldemo/ProfileActivity.java
+++ app/src/main/java/com/example/phili/foodpaldemo/ProfileActivity.java
@@ -2,18 +2,29 @@ package com.example.phili.foodpaldemo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.phili.foodpaldemo.models.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Created by phili on 2018-02-10.
+ * Created by yiren on 2018-02-10.
  */
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
@@ -31,15 +42,34 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     private EditText birthday;
     private EditText about;
 
+    private String uId;
+
     // firebase
-    private FirebaseAuth mAuth;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference mDatabaseGroup;
+    private DatabaseReference mDatabaseUsers;
+
+    private FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
 
-        mAuth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        if (firebaseAuth.getCurrentUser() == null) {
+            //Finish the activity
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        uId = user.getUid();
+        mDatabaseUsers = firebaseDatabase.getReference("users").child(uId);
+
+
+
 
         imageViewedit = findViewById(R.id.editprofile);
         imageViewphoto = findViewById(R.id.editphoto);
@@ -66,9 +96,6 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         username.setFocusableInTouchMode(true);
         username.requestFocus();
 
-        email.setFocusable(true);
-        email.setFocusableInTouchMode(true);
-
         gender.setFocusable(true);
         gender.setFocusableInTouchMode(true);
 
@@ -91,9 +118,6 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         username.setFocusable(false);
         username.setFocusableInTouchMode(false);
 
-        email.setFocusable(false);
-        email.setFocusableInTouchMode(false);
-
         gender.setFocusable(false);
         gender.setFocusableInTouchMode(false);
 
@@ -105,6 +129,23 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         about.setFocusable(false);
         about.setFocusableInTouchMode(false);
+        //Get current value
+        String uName = username.getText().toString().trim();
+        String uEmail = email.getText().toString().trim();
+        String uGender = gender.getText().toString().trim();
+        String uBirthday = birthday.getText().toString().trim();
+        String uMajor = major.getText().toString().trim();
+        String uAbout = about.getText().toString().trim();
+
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        String uId = firebaseUser.getUid();
+
+        //Map<String, Boolean> members = new HashMap<>();
+
+        User user = new User(uId, uName, uEmail, uMajor, uGender, uBirthday,uAbout);
+        mDatabaseUsers.child(uId).setValue(user);
+
+        Toast.makeText(this, "Changes submitted", Toast.LENGTH_LONG).show();
 
     }
     @Override
@@ -122,5 +163,28 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     protected void onStart() {
         super.onStart();
+
+        mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User currentUser = dataSnapshot.getValue(User.class);
+
+                username.setText(currentUser.getUserName());
+                email.setText(currentUser.getUserEmailAddress());
+                gender.setText(currentUser.getUserGender());
+                birthday.setText(currentUser.getUserBirthday());
+                major.setText(currentUser.getUserMajor());
+                about.setText(currentUser.getSelfDescription());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
+
+
 }
