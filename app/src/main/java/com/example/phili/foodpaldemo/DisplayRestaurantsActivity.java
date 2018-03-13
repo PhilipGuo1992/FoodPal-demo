a5cd6637e46d81a79ccaaa6987e445cf2d42ffff
Zongming Liu <zongmingxliu@gmail.com>
2018-03-13 02:13:51 -0300

    Implement partial list of restaurant activity and ui

--- app/src/main/java/com/example/phili/foodpaldemo/DisplayRestaurantsActivity.java
+++ app/src/main/java/com/example/phili/foodpaldemo/DisplayRestaurantsActivity.java
@@ -1,13 +1,26 @@
 package com.example.phili.foodpaldemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayRestaurantsActivity extends AppCompatActivity {
+
+    private TextView txt_display_restaurant_heading;
+    private String city, cuisine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_restaurants);
+
+        txt_display_restaurant_heading = findViewById(R.id.txt_display_restaurant_heading);
+
+        Intent intent = getIntent();
+        city = intent.getStringExtra(DiscoverRestaurantActivity.MESSAGE_CITY);
+        cuisine = intent.getStringExtra(DiscoverRestaurantActivity.MESSAGE_CUISINE);
+
+        txt_display_restaurant_heading.setText(cuisine.concat(" in ".concat(city)));
     }
 }
