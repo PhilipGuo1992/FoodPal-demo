5ed21cd2e2761bb1e615fa0a774fde93f8bd4e0f
Taani0727 <tanishkacs4061@gmail.com>
2018-03-30 23:55:23 -0300

    resolving conflicts

--- app/src/main/java/com/example/phili/foodpaldemo/RegisterActivity.java
+++ app/src/main/java/com/example/phili/foodpaldemo/RegisterActivity.java
@@ -22,6 +22,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 /**
  * Created by yiren on 2018-02-10.
