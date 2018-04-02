6f0906f8827171a725c4e8a7b7a297fca9187774
Zongming Liu <zongmingxliu@gmail.com>
2018-04-02 04:17:02 -0300

    In progress

---
+++ app/src/main/java/com/example/phili/foodpaldemo/RestaurantInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.phili.foodpaldemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_info);
+    }
+}
