82d7b3538e67175ad7811bcf05874c3cc8e20744
Unknown <q852523875@gmail.com>
2018-04-05 18:14:33 -0300

    notification complete

--- app/src/main/java/com/example/phili/foodpaldemo/Function/NotificationOpenedHandler.java
+++ app/src/main/java/com/example/phili/foodpaldemo/Function/NotificationOpenedHandler.java
@@ -1,6 +1,7 @@
 package com.example.phili.foodpaldemo.Function;
 
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.example.phili.foodpaldemo.MainHomeActivity;
@@ -10,13 +11,13 @@ import com.onesignal.OneSignal;
 
 import org.json.JSONObject;
 
-import static android.support.v4.content.ContextCompat.startActivity;
 
 /**
  * Created by wzb on 2018/4/5.
  */
 
-public class NotificationOpenedHandler implements OneSignal.NotificationOpenedHandler {
+public class NotificationOpenedHandler extends AppCompatActivity
+        implements OneSignal.NotificationOpenedHandler {
     // This fires when a notification is opened by tapping on it.
 
     //https://documentation.onesignal.com/docs/android-native-sdk#section--notificationopenedhandler-
@@ -35,18 +36,10 @@ public class NotificationOpenedHandler implements OneSignal.NotificationOpenedHa
         if (actionType == OSNotificationAction.ActionType.ActionTaken)
             Log.i("OneSignalExample", "Button pressed with id: " + result.action.actionID);
 
-        // The following can be used to open an Activity of your choice.
-        // Replace - getApplicationContext() - with any Android Context.
         Intent intent = new Intent(getApplicationContext(), MainHomeActivity.class);
-        // intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
 
-        // Add the following to your AndroidManifest.xml to prevent the launching of your main Activity
-        //   if you are calling startActivity above.
-     /*
-        <application ...>
-          <meta-data android:name="com.onesignal.NotificationOpened.DEFAULT" android:value="DISABLE" />
-        </application>
-     */
+
     }
 }
