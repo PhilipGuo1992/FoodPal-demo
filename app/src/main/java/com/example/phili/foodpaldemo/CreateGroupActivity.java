ebc605277ce5d5a2e836fc7b2fab16b577637323
Yi Ren <yiren1105@gmail.com>
2018-04-06 18:49:44 -0300

    time picker view changed

--- app/src/main/java/com/example/phili/foodpaldemo/CreateGroupActivity.java
+++ app/src/main/java/com/example/phili/foodpaldemo/CreateGroupActivity.java
@@ -59,9 +59,9 @@ public class CreateGroupActivity extends AppCompatActivity implements View.OnCli
 
     //EditText on this page
     private EditText editTextGroupName;
-    private EditText editTextTime;
+    //private EditText editTextTime;
     private EditText editTextRestaurant;
-    private TextView textViewEmail;
+    private TextView editTextTime;
     private EditText editTextDescription;
     private Button btnCreate;
 
